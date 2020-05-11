4e74f686d3c08ff378a60af7c6457c75a6d4c992
nomboloneil <n.bateman@nombolo.com>
2020-05-10 17:44:17 -0700

    updated metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'The personal blog of Leslie Bateman',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
-  },
+      'The musings of Leslie Bateman',
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
